1b036b7e700cfe4bad6c9f84fab550c5d81d61ad
yinshi2018 <shiyin_dalian@163.com>
2025-08-27 17:07:34 +0200

    data

--- MF_DR/Z_highT/highT.py
+++ MF_DR/Z_highT/highT.py
@@ -21,6 +21,14 @@ Zdata=np.loadtxt('./Zall.dat')
 Zdata2=np.loadtxt('./Zall2.dat')
 highTdata=np.loadtxt('./ZhighTdata.dat')
 
+Zvac=np.loadtxt('./Zvac.dat')
+Zph=np.loadtxt('./ZPH.dat')
+Zca=np.loadtxt('./ZCA.dat')
+
+Zvacmu320=np.loadtxt('./Zvacmu320.dat')
+Zphmu320=np.loadtxt('./ZPHmu320.dat')
+Zcamu320=np.loadtxt('./ZCAmu320.dat')
+
 p = np.linspace(0, 10000, 10001)
 Thigh = np.linspace(10, 2000, 200)
 
@@ -28,16 +36,18 @@ Thigh = np.linspace(10, 2000, 200)
 fig=plt.figure(figsize=(4.5, 3.5))
 #fig=plt.figure()
 ax1=plt.subplot(1,1,1)
-ax1.plot(p,Zdata,c='k',linewidth=2.5,alpha=0.6,label=r'$Z^\perp_\pi$ at $\mu=0$')
-#ax1.plot(Zdata2[:,0],Zdata2[:,1],c='k',linewidth=2.5,alpha=0.6)
-ax1.plot(Thigh[100:200],highTdata[100:200],c='r',dashes=[1,1],linewidth=2.5,alpha=0.7,label=r'Large-T limit')
-#ax1.plot([0,20],[0,0],c='k',dashes=(1,1))
-#ax1.text(38, -2.1, r'$\mu=0$', fontsize=15, color='black')
-#ax1.text(10, 585, r'$(0,600)$', fontsize=12, color='red')
-#ax1.text(200, 620, r'$(282,707)$', fontsize=12, color='red')
+ax1.plot(p,Zdata,c='k',linewidth=2.5,alpha=0.6,label=r'$Z^\perp_\pi$')
+
+ax1.plot(Thigh[40:200],highTdata[40:200],c='r',dashes=[2,1],linewidth=2.5,alpha=0.7,label=r'Large-T limit')
+
+ax1.plot(Thigh,Zvac,c='b',linewidth=2.5,alpha=0.5,label=r'Vacuum part')
+ax1.plot(Thigh,Zca+Zvac,c='m',dashes=[3,1,1,1],linewidth=2.5,alpha=0.5,label=r'CA part')
+ax1.plot(Thigh,Zph,c='g',dashes=[1,1],linewidth=2.5,alpha=0.5,label=r'PH part')
+ax1.text(28,4,r'$\mu=0$',fontsize=15)
+
 ax1.set_xscale('log')
-ax1.axis([10.,2000,-4,2.])
-ax1.legend(loc=0,fontsize=12,frameon=False,shadow=True,handlelength=3.,borderpad=0.5,borderaxespad=1,numpoints=1,scatterpoints=1)
+ax1.axis([10.,2000,-5,15.])
+ax1.legend(loc=0,fontsize=10,frameon=False,shadow=True,handlelength=3.,borderpad=0.5,borderaxespad=1,numpoints=1,scatterpoints=1)
 
 ax1.set_ylabel(r'$Z^{\perp}_\pi(p=0)$', fontsize=13, color='black')
 ax1.set_xlabel(r'$T\,[\mathrm{MeV}]$', fontsize=13, color='black')
@@ -46,6 +56,31 @@ for label in ax1.xaxis.get_ticklabels():
 for label in ax1.yaxis.get_ticklabels():
     label.set_fontsize(10)
 
-fig.subplots_adjust(top=0.9, bottom=0.14, left=0.13, right=0.95, hspace=0.35, wspace=0.35)
+fig.subplots_adjust(top=0.9, bottom=0.14, left=0.143, right=0.95, hspace=0.35, wspace=0.35)
 
 fig.savefig("./highT.pdf")
+#####################################################
+# Create figure
+fig=plt.figure(figsize=(4.5, 3.5))
+ax1=plt.subplot(1,1,1)
+ax1.plot(Thigh,Zvacmu320+Zphmu320+Zcamu320,c='k',linewidth=2.5,alpha=0.6,label=r'$Z^\perp_\pi$ at $\mu=320$ MeV')
+
+ax1.plot(Thigh,Zvacmu320,c='b',linewidth=2.5,alpha=0.5,label=r'Vacuum part')
+ax1.plot(Thigh,Zcamu320+Zvacmu320,c='m',dashes=[3,1,1,1],linewidth=2.5,alpha=0.5,label=r'CA part')
+ax1.plot(Thigh,Zphmu320,c='g',dashes=[1,1],linewidth=2.5,alpha=0.5,label=r'PH part')
+ax1.text(300,4,r'$\mu=320$ $\mathrm{MeV}$',fontsize=15)
+
+ax1.set_xscale('log')
+ax1.axis([10.,2000,-2,8])
+#ax1.legend(loc=0,fontsize=10,frameon=False,shadow=True,handlelength=3.,borderpad=0.5,borderaxespad=1,numpoints=1,scatterpoints=1)
+
+ax1.set_ylabel(r'$Z^{\perp}_\pi(p=0)$', fontsize=13, color='black')
+ax1.set_xlabel(r'$T\,[\mathrm{MeV}]$', fontsize=13, color='black')
+for label in ax1.xaxis.get_ticklabels():
+    label.set_fontsize(10)
+for label in ax1.yaxis.get_ticklabels():
+    label.set_fontsize(10)
+
+fig.subplots_adjust(top=0.9, bottom=0.14, left=0.143, right=0.95, hspace=0.35, wspace=0.35)
+
+fig.savefig("./highTmu320.pdf")
