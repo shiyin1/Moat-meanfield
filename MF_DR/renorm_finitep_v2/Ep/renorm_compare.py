b21d3b239515d6cc16f03422f6f09864e268d16b
yinshi2018 <shiyin_dalian@163.com>
2025-09-03 19:41:44 +0200

    data

--- MF_DR/renorm_finitep_v2/Ep/renorm_compare.py
+++ MF_DR/renorm_finitep_v2/Ep/renorm_compare.py
@@ -19,6 +19,7 @@ mpl.style.use('classic')
 # Data for plotting
 v1=np.loadtxt('./renormv1.dat')
 v2=np.loadtxt('./renormv2.dat')
+v3=np.loadtxt('./renormM500.dat')
 ps=np.loadtxt('./ps.dat')
 
 # Create figure
@@ -27,9 +28,10 @@ fig=plt.figure(figsize=(4.5, 3.5))
 ax1=plt.subplot(1,1,1)
 
 ax1.plot(ps, v1/10**6,color='k',alpha=0.6,linewidth=2,label=r'$\overline{MS}$') 
+ax1.plot(ps, v3/10**6,color='k',dashes=[2,2],alpha=0.6,linewidth=2,label=r'$\overline{MS}\,\,\,M=500\,\mathrm{MeV}\,\,\,\mathrm{unrenormalized}$') 
 ax1.plot(ps, v2/10**6,color='b',alpha=0.6,linewidth=2,label=r'$\overline{MS}+\mathrm{Vacuum\,\,Subtraction}$') 
 ax1.plot([0,2000],[0,0],dashes=[1,1],color='k',alpha=0.2,linewidth=1) 
-ax1.text(100, 1.8, r'$\mu=400\,\mathrm{MeV},\,T=10\,\mathrm{MeV}$', fontsize=12, color='k')
+ax1.text(100, 1.5, r'$\mu=400\,\mathrm{MeV},\,T=10\,\mathrm{MeV}$', fontsize=12, color='k')
 
 ax1.set_ylabel(r'$\Sigma_\pi(0,|\mathbf{p}|)\,[\times 10^6\,\mathrm{MeV}^2]$', fontsize=13, color='black')
 ax1.set_xlabel(r'$|\mathbf{p}|\,[\mathrm{MeV}]$', fontsize=13, color='black')
